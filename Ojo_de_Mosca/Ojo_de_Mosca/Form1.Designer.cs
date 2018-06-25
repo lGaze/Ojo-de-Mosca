20b98cbc2ff4086ac60cab7cc7a02687ee5922b3
BioAbner <bioabner@hotmail.com>
2018-06-25 02:33:37 -0500

    todo lo demas

--- Ojo_de_Mosca/Ojo_de_Mosca/Form1.Designer.cs
+++ Ojo_de_Mosca/Ojo_de_Mosca/Form1.Designer.cs
@@ -31,8 +31,9 @@
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btnDibujar = new System.Windows.Forms.Button();
             this.nudePuntos = new System.Windows.Forms.NumericUpDown();
+            this.btnDibujar = new System.Windows.Forms.Button();
+            this.ckbUseThreads = new System.Windows.Forms.CheckBox();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
@@ -51,7 +52,7 @@
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(506, 414);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(644, 414);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // pictureBox1
@@ -59,34 +60,22 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Location = new System.Drawing.Point(3, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(384, 408);
+            this.pictureBox1.Size = new System.Drawing.Size(490, 408);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.LoadCompleted += new System.ComponentModel.AsyncCompletedEventHandler(this.pictureBox1_LoadCompleted);
-            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
+            this.pictureBox1.Resize += new System.EventHandler(this.PictureBox1_Resize);
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.ckbUseThreads);
             this.panel1.Controls.Add(this.nudePuntos);
             this.panel1.Controls.Add(this.btnDibujar);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(393, 3);
+            this.panel1.Location = new System.Drawing.Point(499, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(110, 408);
+            this.panel1.Size = new System.Drawing.Size(142, 408);
             this.panel1.TabIndex = 1;
-            // 
-            // btnDibujar
-            // 
-            this.btnDibujar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnDibujar.Location = new System.Drawing.Point(13, 171);
-            this.btnDibujar.Name = "btnDibujar";
-            this.btnDibujar.Size = new System.Drawing.Size(88, 52);
-            this.btnDibujar.TabIndex = 0;
-            this.btnDibujar.Text = "Dibujar";
-            this.btnDibujar.UseVisualStyleBackColor = true;
-            this.btnDibujar.Click += new System.EventHandler(this.btnDibujar_Click);
             // 
             // nudePuntos
             // 
@@ -106,7 +95,7 @@
             0,
             0});
             this.nudePuntos.Name = "nudePuntos";
-            this.nudePuntos.Size = new System.Drawing.Size(88, 38);
+            this.nudePuntos.Size = new System.Drawing.Size(120, 38);
             this.nudePuntos.TabIndex = 1;
             this.nudePuntos.Value = new decimal(new int[] {
             5,
@@ -114,17 +103,45 @@
             0,
             0});
             // 
+            // btnDibujar
+            // 
+            this.btnDibujar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDibujar.Location = new System.Drawing.Point(13, 171);
+            this.btnDibujar.Name = "btnDibujar";
+            this.btnDibujar.Size = new System.Drawing.Size(120, 52);
+            this.btnDibujar.TabIndex = 0;
+            this.btnDibujar.Text = "Dibujar";
+            this.btnDibujar.UseVisualStyleBackColor = true;
+            this.btnDibujar.Click += new System.EventHandler(this.BtnDibujar_Click);
+            // 
+            // ckbUseThreads
+            // 
+            this.ckbUseThreads.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ckbUseThreads.AutoSize = true;
+            this.ckbUseThreads.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.ckbUseThreads.Location = new System.Drawing.Point(8, 78);
+            this.ckbUseThreads.Name = "ckbUseThreads";
+            this.ckbUseThreads.Size = new System.Drawing.Size(125, 21);
+            this.ckbUseThreads.TabIndex = 2;
+            this.ckbUseThreads.Text = "Magias Negras";
+            this.ckbUseThreads.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(506, 414);
+            this.ClientSize = new System.Drawing.Size(644, 414);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.tableLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nudePuntos)).EndInit();
             this.ResumeLayout(false);
 
@@ -137,6 +154,7 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.NumericUpDown nudePuntos;
         private System.Windows.Forms.Button btnDibujar;
+        private System.Windows.Forms.CheckBox ckbUseThreads;
     }
 }
 
--- Ojo_de_Mosca/Ojo_de_Mosca/Form1.cs
+++ Ojo_de_Mosca/Ojo_de_Mosca/Form1.cs
@@ -1,73 +1,226 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Threading;
 
 namespace Ojo_de_Mosca
 {
     public partial class Form1 : Form
     {
-       
-        
+        int numPoints;
+        List<Point[]> lines = new List<Point[]>();
+        List<Color[]> colors = new List<Color[]>();
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        
-      
-
-        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        private Color HsvToRgb(double h, double S, double V)
         {
-          
-        
-            PictureBox lienzo = sender as PictureBox;
-            
-            Pen pluma = new Pen(Color.Black, 2f);
-            var graphics = e.Graphics;
-            lienzo.Image = null;
-            int numPuntos = Convert.ToInt32(nudePuntos.Value);
-            bool esPar = numPuntos % 2 == 0;
-            int Radio = Math.Min(lienzo.Width, lienzo.Height) * 4 / 10;
+            double H = h;
+            while (H < 0) { H += 360; };
+            while (H >= 360) { H -= 360; };
+            double R, G, B;
+            if (V <= 0)
+            { R = G = B = 0; }
+            else if (S <= 0)
+            {
+                R = G = B = V;
+            }
+            else
+            {
+                double hf = H / 60.0;
+                int i = (int)Math.Floor(hf);
+                double f = hf - i;
+                double pv = V * (1 - S);
+                double qv = V * (1 - S * f);
+                double tv = V * (1 - S * (1 - f));
+                switch (i)
+                {
+
+                    // Red is the dominant color
+
+                    case 0:
+                        R = V;
+                        G = tv;
+                        B = pv;
+                        break;
+
+                    // Green is the dominant color
+
+                    case 1:
+                        R = qv;
+                        G = V;
+                        B = pv;
+                        break;
+                    case 2:
+                        R = pv;
+                        G = V;
+                        B = tv;
+                        break;
+
+                    // Blue is the dominant color
+
+                    case 3:
+                        R = pv;
+                        G = qv;
+                        B = V;
+                        break;
+                    case 4:
+                        R = tv;
+                        G = pv;
+                        B = V;
+                        break;
+
+                    // Red is the dominant color
+
+                    case 5:
+                        R = V;
+                        G = pv;
+                        B = qv;
+                        break;
+
+                    // Just in case we overshoot on our math by a little, we put these here. Since its a switch it won't slow us down at all to put these here.
+
+                    case 6:
+                        R = V;
+                        G = tv;
+                        B = pv;
+                        break;
+                    case -1:
+                        R = V;
+                        G = pv;
+                        B = qv;
+                        break;
+
+                    // The color is not defined, we should throw an error.
+
+                    default:
+                        //LFATAL("i Value error in Pixel conversion, Value is %d", i);
+                        R = G = B = V; // Just pretend its black/white
+                        break;
+                }
+            }
+
+            return Color.FromArgb(
+                Clamp((int)(R * 255.0)), 
+                Clamp((int)(G * 255.0)), 
+                Clamp((int)(B * 255.0)));
+        }
+
+        private int Clamp(int i)
+        {
+            if (i < 0) return 0;
+            if (i > 255) return 255;
+            return i;
+        }
+
+        private void RecalculateLines()
+        {
+            PictureBox lienzo = pictureBox1;
+            bool esPar = numPoints % 2 == 0;
+            double Radio = Math.Min(lienzo.Width, lienzo.Height) / 2.0;
             int x_centro = lienzo.Width / 2;
             int y_centro = lienzo.Height / 2;
-            Point[] puntos = new Point[numPuntos];
-            
-            
-            for (int i = 0; i < numPuntos; i++)
+            Point[] puntos = new Point[numPoints];
+            Color[] colores = new Color[numPoints];
+
+            if (ckbUseThreads.Checked)
             {
-                puntos[i].X = Convert.ToInt32(Math.Sin(i * (Math.PI * 2) / numPuntos) * Radio) + x_centro;
-                puntos[i].Y = Convert.ToInt32(Math.Cos(i * (Math.PI * 2) / numPuntos) * Radio) + y_centro;
+                int half = (int)Math.Ceiling(numPoints / 2.0);
+                Thread thread1 = new Thread(new ThreadStart(() => ThreadRecalculateLines(0, half, Radio, x_centro, y_centro, ref puntos, ref colores)));
+                Thread thread2 = new Thread(new ThreadStart(() => ThreadRecalculateLines(half, numPoints, Radio, x_centro, y_centro, ref puntos, ref colores)));
+                thread1.Start();
+                thread2.Start();
+                thread1.Join();
+                thread2.Join();
             }
-            for (int i = 0; i < numPuntos - 1; i++)
+            else
             {
-                for (int j = i + 1; j < numPuntos; j++)
+                for (int i = 0; i < numPoints; i++)
                 {
-                    if (esPar && i + numPuntos / 2 == j)
+                    double radians = i * (Math.PI * 2.0) / numPoints;
+                    double angle = i * 360.0 / numPoints;
+
+                    puntos[i].X = Convert.ToInt32(Math.Sin(radians) * Radio) + x_centro;
+                    puntos[i].Y = Convert.ToInt32(Math.Cos(radians) * Radio) + y_centro;
+
+                    colores[i] = HsvToRgb(angle, 1, 1);
+                }
+            }
+
+            lines.Clear();
+            colors.Clear();
+            for (int i = 0; i < numPoints - 1; i++)
+            {
+                for (int j = i + 1; j < numPoints; j++)
+                {
+                    if (esPar && i + numPoints / 2 == j)
                     {
                         continue;
                     }
-                    graphics.DrawLine(pluma, puntos[i], puntos[j]);
+                    lines.Add(new Point[] { puntos[i], puntos[j] });
+                    colors.Add(new Color[] { colores[i], colores[j] });
                 }
-
             }
-                
         }
 
-        private void btnDibujar_Click(object sender, EventArgs e)
+        private void ThreadRecalculateLines(int ini, int fin, double radio, int x_centro, int y_centro, ref Point[] puntos, ref Color[] colores)
         {
-            
-            
+            for (int i = ini; i < fin; i++)
+            {
+                double radians = i * (Math.PI * 2.0) / numPoints;
+                double angle = i * 360.0 / numPoints;
+
+                puntos[i].X = Convert.ToInt32(Math.Sin(radians) * radio) + x_centro;
+                puntos[i].Y = Convert.ToInt32(Math.Cos(radians) * radio) + y_centro;
+
+                colores[i] = HsvToRgb(angle, 1, 1);
+            }
         }
 
-        private void pictureBox1_LoadCompleted(object sender, AsyncCompletedEventArgs e)
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
         {
+            if (numPoints <= 0) return;
+
             PictureBox lienzo = sender as PictureBox;
-            lienzo.InitialImage = null;
+
+            var graphics = e.Graphics;
+
+            Random r = new Random();
+
+            var linesAndColors = lines.Zip(colors, (n, w) => new { Line = n, Color = w });
+            foreach (var lineColor in linesAndColors)
+            {
+                using (Brush aGradientBrush = new LinearGradientBrush(lineColor.Line[0], lineColor.Line[1], lineColor.Color[0], lineColor.Color[0]))
+                {
+                    using (Pen aGradientPen = new Pen(aGradientBrush))
+                    {
+                        graphics.DrawLine(aGradientPen, lineColor.Line[0], lineColor.Line[1]);
+                    }
+                }
+            }
+        }
+
+        private void BtnDibujar_Click(object sender, EventArgs e)
+        {
+            numPoints = Convert.ToInt32(nudePuntos.Value);
+            RecalculateLines();
+            pictureBox1.Refresh();
+        }
+
+        private void PictureBox1_Resize(object sender, EventArgs e)
+        {
+            if (numPoints > 0)
+            {
+                RecalculateLines();
+                pictureBox1.Refresh();
+            }
         }
     }
 }
