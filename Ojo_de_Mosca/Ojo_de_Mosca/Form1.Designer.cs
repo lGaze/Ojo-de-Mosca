5b2498055ba648a7569bbbe3dbcee87071522b2a
Victor Hugo Flores <idv16c.vflores@uartesdigitles.edu.mx>
2018-06-24 23:56:35 -0500

    puch chnger simetrical

--- Ojo_de_Mosca/Ojo_de_Mosca/Form1.Designer.cs
+++ Ojo_de_Mosca/Ojo_de_Mosca/Form1.Designer.cs
@@ -28,12 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnDibujar = new System.Windows.Forms.Button();
+            this.nudePuntos = new System.Windows.Forms.NumericUpDown();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudePuntos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 77.0751F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 22.9249F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(506, 414);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(384, 408);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.LoadCompleted += new System.ComponentModel.AsyncCompletedEventHandler(this.pictureBox1_LoadCompleted);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.nudePuntos);
+            this.panel1.Controls.Add(this.btnDibujar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(393, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(110, 408);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnDibujar
+            // 
+            this.btnDibujar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDibujar.Location = new System.Drawing.Point(13, 171);
+            this.btnDibujar.Name = "btnDibujar";
+            this.btnDibujar.Size = new System.Drawing.Size(88, 52);
+            this.btnDibujar.TabIndex = 0;
+            this.btnDibujar.Text = "Dibujar";
+            this.btnDibujar.UseVisualStyleBackColor = true;
+            this.btnDibujar.Click += new System.EventHandler(this.btnDibujar_Click);
+            // 
+            // nudePuntos
+            // 
+            this.nudePuntos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.nudePuntos.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudePuntos.Location = new System.Drawing.Point(13, 117);
+            this.nudePuntos.Maximum = new decimal(new int[] {
+            150,
+            0,
+            0,
+            0});
+            this.nudePuntos.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nudePuntos.Name = "nudePuntos";
+            this.nudePuntos.Size = new System.Drawing.Size(88, 38);
+            this.nudePuntos.TabIndex = 1;
+            this.nudePuntos.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 414);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nudePuntos)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown nudePuntos;
+        private System.Windows.Forms.Button btnDibujar;
     }
 }
 
--- Ojo_de_Mosca/Ojo_de_Mosca/Form1.cs
+++ Ojo_de_Mosca/Ojo_de_Mosca/Form1.cs
@@ -11,9 +11,63 @@ namespace Ojo_de_Mosca
 {
     public partial class Form1 : Form
     {
+       
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        
+      
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+          
+        
+            PictureBox lienzo = sender as PictureBox;
+            
+            Pen pluma = new Pen(Color.Black, 2f);
+            var graphics = e.Graphics;
+            lienzo.Image = null;
+            int numPuntos = Convert.ToInt32(nudePuntos.Value);
+            bool esPar = numPuntos % 2 == 0;
+            int Radio = Math.Min(lienzo.Width, lienzo.Height) * 4 / 10;
+            int x_centro = lienzo.Width / 2;
+            int y_centro = lienzo.Height / 2;
+            Point[] puntos = new Point[numPuntos];
+            
+            
+            for (int i = 0; i < numPuntos; i++)
+            {
+                puntos[i].X = Convert.ToInt32(Math.Sin(i * (Math.PI * 2) / numPuntos) * Radio) + x_centro;
+                puntos[i].Y = Convert.ToInt32(Math.Cos(i * (Math.PI * 2) / numPuntos) * Radio) + y_centro;
+            }
+            for (int i = 0; i < numPuntos - 1; i++)
+            {
+                for (int j = i + 1; j < numPuntos; j++)
+                {
+                    if (esPar && i + numPuntos / 2 == j)
+                    {
+                        continue;
+                    }
+                    graphics.DrawLine(pluma, puntos[i], puntos[j]);
+                }
+
+            }
+                
+        }
+
+        private void btnDibujar_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void pictureBox1_LoadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            PictureBox lienzo = sender as PictureBox;
+            lienzo.InitialImage = null;
         }
     }
 }
